83a13ba7e7c73dc1f03582e25177100ae622322a
Pascal <ro020701@gmail.com>
2023-01-07 00:03:57 +0800

    2023.01.07 add some comment

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ def label_fix(i, j):
         imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1] = temp
 
 
+# # # # # # # # # # #
 if __name__ == '__main__':
     # Read the image for dilation
     img = cv2.imread("gData/nchu.jpg", cv2.IMREAD_GRAYSCALE)
     # Acquire size of the image
     p, q = img.shape
     # Show the image
-    cv2.namedWindow('ii', 0)
+    # cv2.namedWindow('ii', 0)
     cv2.imshow('ii', img)
 
     # Define new image to store the pixels of dilated image
@@ -26,46 +27,30 @@ if __name__ == '__main__':
     constant1 = 1
     group_label = 1
 
-    # Dilation operation without using inbuilt CV2 function
+    # Start
     for i in range(constant1, p - constant1):
         for j in range(constant1, q - constant1):
             if img[i, j] > 0:
                 temp = img[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1]
                 if np.max(imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1]) == 0:
-                    # print('a', i, j)
-                    # print(imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1])
-
                     product = temp * SED
                     product[product > 0] = group_label
                     imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1] = product
-                    # imgDilate[i, j] = np.max(product)
-
-                    # print(temp)
-                    # print(product)
-                    # print(imgDilate)
-
                     group_label += 1
 
                 else:
                     min_temp = imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1]
-                    # min_temp[min_temp == 0] = group_label
-                    # min_label = np.min(min_temp)
                     min_label = min(filter(lambda x: x > 0, min_temp.reshape(-1, )))
                     product = temp * SED
-                    # product[product > 0] = min_label if min_label <= group_label else group_label
                     product[product > 0] = min_label
                     imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1] = product
-                    # if i >= 12 and j >= 31:
-                    #     print('b', i, j)
-                    #     print(imgDilate[i - constant1:i + constant1 + 1, j - constant1:j + constant1 + 1])
-                    #     print(temp)
-                    #     print(product)
-                    #     print(imgDilate)
 
+    # label fix
     for i in range(p - constant1, constant1, -1):
         for j in range(q - constant1, constant1, -1):
             label_fix(i, j)
 
+    # label fix sec
     for i in range(constant1, p - constant1):
         for j in range(constant1, q - constant1):
             label_fix(i, j)
@@ -79,12 +64,10 @@ if __name__ == '__main__':
                     total_cal[attr_str] += 1
                 else:
                     total_cal[attr_str] = 1
-
             imgDilate[i][j] = imgDilate[i][j] * 20
 
-    # print(group_label)
     print(total_cal)
-    cv2.namedWindow('test', 0)
+    # cv2.namedWindow('test', 0)
     cv2.imshow('test', imgDilate)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
